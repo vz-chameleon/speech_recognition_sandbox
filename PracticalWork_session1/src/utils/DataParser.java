2c2b895291d1ac98dd931d8017fb873c79817e35
GTimothy <timothy.garwood@protonmail.com>
2018-01-25 17:41:18 +0100

    continuation of phonem parser

--- PracticalWork_session1/src/utils/DataParser.java
+++ PracticalWork_session1/src/utils/DataParser.java
@@ -1,29 +1,58 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.AbstractMap.SimpleEntry;;
 
 public class DataParser {
-	
+
 	public DataParser(){
-		
+
 	}
-	
-	public String parsePhonemeFile(String filepath){
-		BufferedReader br = new BufferedReader(new FileReader(file));
+
+	public ArrayList<SimpleEntry<String,String[]>> loadLexicFile(String filepath) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(filepath));
+		ArrayList<SimpleEntry<String,String[]>> LexicPhonemsAList = new ArrayList<>();
+		
 		String line;
 		while ((line = br.readLine()) != null) {
-		   // process the line.
-		   if (line.equals(". "))
-		   {
-		       // Do something with first line
-		       line = br.readLine()
-		       // Do something with second line
-		       line = br.readLine()
-		       // Split up the third line by space 
-		       String split[]= StringUtils.split(line); // split[1] = "Mozart," so you may need to do a little more work there
-		   }
+			String split[]= line.split("\t"); // split[1] = "Mozart," so you may need to do a little more work there
+			String word = split[0];
+			String[] phonems = split[1].split(" ");
+			LexicPhonemsAList.add(new SimpleEntry<String,String[]>(word,phonems));
 		}
 		br.close();
+		return LexicPhonemsAList;
+	}
+	
+	public void checkTestFile(String filepath, ArrayList<SimpleEntry<String,String[]>> LexicPhonemsAList) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(filepath));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(filepath+".testLog"));
+		
+		String line;
+		while ((line = br.readLine()) != null) {
+	
+			String split[]= line.split("\t");
+			String testword = split[0];
+			String[] testphonems = split[1].split(" ");
+			
+//			Call function to test out testword's phonem with our LexicPhonemsAList
+//			String recognisedWord, String[] recognisedWordphonems, in/double cost = getrecognisedWordwithPhonemsAndCost(testphonems,LexicPhonemsAList)
+//			log this to file
+//			logtofile(bw, testword, testphonems, recognisedWord, recognisedWordphonems, cost);
+		}
+		br.close();
+		bw.close();
+	}
+	
+	public void logtofile(Writer filewriter, String motTest, String[] phonesTest, String motReconnu){
+//		log this to file : <mot test> [phones test]  <mot reconnu> [phones ref] ok/err cout align
 	}
 
 	/**
